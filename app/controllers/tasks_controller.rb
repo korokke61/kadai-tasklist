eed12f4bac6deea45535cc69e1eadec899edb588
akio.kirameki <akio.nabekko@gmail.com>
2020-09-22 07:10:54 +0000

    課題再修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -52,6 +52,6 @@ class TasksController < ApplicationController
     private
     
     def task_params
-      params.require(:task).permit(:task, :status)
+      params.require(:task).permit(:content, :status)
     end
 end
